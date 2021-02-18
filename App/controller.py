60ffd58d8978f80f46d59f089b98e5cde32cf78a
valentinajtovar <lv.jimenez@uniandes.edu.co>
2021-02-17 20:40:00 -0500

    En espera de que funcione

--- App/controller.py
+++ App/controller.py
@@ -1,33 +1,10 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
+﻿
 
 import config as cf
 import model
 import csv
 
 
-"""
-El controlador se encarga de mediar entre la vista y el modelo.
-"""
 
 # Inicialización del Catálogo de libros
 def initCatalogo():
@@ -37,12 +14,10 @@ def initCatalogo():
 
 def loadDatos(catalogo):
  
-    loadBooks(catalogo)
+    loadvideos(catalogo)
     loadTags(catalogo)
     loadVideosTags(catalogo)
     sortVideos(catalogo)
-
-# Funciones de ordenamiento
 
 def loadVideos(catalogo):
     
@@ -54,10 +29,29 @@ def loadVideos(catalogo):
 
 def loadTags(catalogo):
    
-    Categoriafile = cf.data_dir + 'GoodReads/Category-id.csv'
+    tagsfile = cf.data_dir + 'GoodReads/Category-id.csv'
     input_file = csv.DictReader(open(tagsfile, encoding='utf-8'))
     for categoria in input_file:
-        model.addCategoria(catalogo, Categoria)
+        model.addVideosTag(catalogo, video)
 
+def loadVideosTags(catalogo):
+    """
+    Carga la información que asocia tags con libros.
+    """
+    videostagsfile = cf.data_dir + 'GoodReads/category-id.csv'
+    input_file = csv.DictReader(open(videostagsfile, encoding='utf-8'))
+    for categoria in input_file:
+        model.addVideosTag(catalog, Videostag)
+
+# Funciones de ordenamiento
+def sortVideos(catalogo):
+   
+    pass
 
 # Funciones de consulta sobre el catálogo
+
+
+def getTendenciaXpais(catalogo, country):
+    
+   pass
+
